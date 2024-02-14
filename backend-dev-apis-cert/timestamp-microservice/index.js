96f5e9af7b7c4dc38d5476e61b3efbfddc5aaaa7
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2024-02-13 23:24:35 -0500

    add: timestamp-microservice

--- backend-dev-apis-cert/timestamp-microservice/index.js
+++ backend-dev-apis-cert/timestamp-microservice/index.js
@@ -6,27 +6,27 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/:date?", (req, res) => {
-  let date = req.params.date || new Date();
+app.get("/api/:input?", (req, res) => {
+  let input = req.params.input || new Date();
     
-  if (!(date instanceof Date) && !date.includes("-")) {
-    date = parseInt(date);
-  }
+  let date = new Date(input);
 
-  let unix = new Date(date).getTime();
-  let utc = new Date(date).toUTCString();
-  let obj =
-    utc == "Invalid Date"
-      ? { error: "Invalid Date" }
-      : { unix: unix, utc: utc };
-
+  if (isNaN(date.getTime()) && !isNaN(input)) 
+      date = new Date(parseInt(input));
+  
+  if (isNaN(date.getTime())) 
+      return res.json({ error: "Invalid Date" });  
+  
+  let unix = date.getTime();
+  let utc = date.toUTCString();
+  let obj = { unix: unix, utc: utc };
   res.json(obj);
 });
 
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
